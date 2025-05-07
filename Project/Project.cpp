507a3b9643b2d3690459bff94994a44ccfcb87f5
dan1lka257 <daniltursunov597@gmail.com>
2025-05-07 17:18:00 +0300

    [Code] Add & Implement Binary Heap for Prim

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -40,19 +40,35 @@ struct Edges {
 template <typename T, typename U>
 struct Graph {
     std::vector<std::vector<std::pair<U, T>>> neig;
-    U n, m;
-    Graph(U n, U m): n(n), m(m) {
-        neig.resize(n + 1, std::vector<std::pair<U, T>> {});
+    U vertexCount, edgeCount;
+    Graph(U vertexCount, U edgeCount): vertexCount(vertexCount), edgeCount(edgeCount) {
+        neig.resize(vertexCount + 1, std::vector<std::pair<U, T>> {});
     }
-    Graph(Edges<T, U>& edges): n(edges.vertexCount), m(edges.edgeCount) {
-        neig.resize(n + 1, std::vector<std::pair<U, T>> {});
+    Graph(Edges<T, U>& edges): vertexCount(edges.vertexCount), edgeCount(edges.edgeCount) {
+        neig.resize(vertexCount + 1, std::vector<std::pair<U, T>> {});
         for (auto edge : edges.edges) {
             neig[edge.from].emplace_back(std::make_pair(edge.to, edge.cost));
             neig[edge.to].emplace_back(std::make_pair(edge.from, edge.cost));
         }
     }
+    bool operator==(const Graph<T, U>& another_graph) const {
+        if (vertexCount != another_graph.vertexCount || edgeCount != another_graph.edgeCount) {
+            return false;
+        }
+        for (U i = 1; i < static_cast<U>(neig.size()); ++i) {
+            if (neig[i].size() != another_graph.neig[i].size()) {
+                return false;
+            }
+            for (U j = 1; j < static_cast<U>(neig[i].size()); ++j) {
+                if (neig[i][j] != another_graph.neig[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     void input() {
-        for (U i = 0; i < m; ++i) {
+        for (U i = 0; i < edgeCount; ++i) {
             U from, to;
             T cost;
             std::cin >> from >> to >> cost;
@@ -71,35 +87,96 @@ struct Graph {
     }
 };
 
-// TODO: IMPLEMENT BINARY HEAP INSTEAD LINEAR SEARCH
-template <typename U>
-U find_min(std::vector<U>& dist, std::vector<U>& used) {
-    U min_value = INF<U>;
-    U min_ind = -1;
-    for (U i = 0; i < static_cast<U>(dist.size()); ++i) {
-        if (min_value >= dist[i] && !used[i]) {
-            min_value = dist[i];
-            min_ind = i;
+template <typename T>
+class BinaryHeap {
+private:
+    std::vector<T> heap;
+
+    size_t parent(size_t i) const { return (i - 1) / 2; }
+    size_t left_child(size_t i) const { return 2 * i + 1; }
+    size_t right_child(size_t i) const { return 2 * i + 2; }
+
+    void sift_up(size_t i) {
+        while (i > 0 && heap[parent(i)] > heap[i]) {
+            std::swap(heap[parent(i)], heap[i]);
+            i = parent(i);
         }
     }
-    used[min_ind] = 1;
-    return min_ind;
+
+    void sift_down(size_t i) {
+        size_t min_index = i;
+        size_t left = left_child(i);
+        size_t right = right_child(i);
+
+        if (left < heap.size() && heap[left] < heap[min_index]) {
+            min_index = left;
+        }
+        if (right < heap.size() && heap[right] < heap[min_index]) {
+            min_index = right;
+        }
+        if (i != min_index) {
+            std::swap(heap[i], heap[min_index]);
+            sift_down(min_index);
+        }
+    }
+
+public:
+    BinaryHeap() = default;
+
+    bool empty() const { return heap.empty(); }
+
+    const T& top() const {
+        if (empty()) {
+            throw std::out_of_range("Heap is empty");
+        }
+        return heap.front();
+    }
+
+    void push(const T& value) {
+        heap.push_back(value);
+        sift_up(heap.size() - 1);
+    }
+
+    void pop() {
+        if (empty()) {
+            throw std::out_of_range("Heap is empty");
+        }
+        heap[0] = heap.back();
+        heap.pop_back();
+        if (!empty()) {
+            sift_down(0);
+        }
+    }
+};
+
+template <typename T, typename U>
+U find_min(BinaryHeap<std::pair<T, U>>& dist, std::vector<U>& used) {
+    while (used[dist.top().second]) {
+        dist.pop();
+    }
+    U min_dist_ind = dist.top().second;
+    used[min_dist_ind] = 1;
+    return min_dist_ind;
 }
 
 template <typename T, typename U>
 Graph<T, U> Prim(Graph<T, U>& graph) {
-    Graph<T, U> new_graph(graph.n, 0);
-    std::vector<std::pair<T, U>> parent(graph.n + 1, std::make_pair(-1, -1));
-    std::vector<U> dist(graph.n + 1, INF<U>);
-    std::vector<U> used(graph.n + 1, 0);
-    dist[1] = 0;
+    Graph<T, U> new_graph(graph.vertexCount, 0);
+    std::vector<std::pair<T, U>> parent(graph.vertexCount + 1, std::make_pair(-1, -1));
+    std::vector<U> used(graph.vertexCount + 1, 0);
+    BinaryHeap<std::pair<T, U>> dist;
+    dist.push({ 0, 1 }); 
     for (U i = 0; i < static_cast<U>(graph.neig.size()); ++i) {
-        U min_ind = find_min(dist, used);
-        for (U j = 0; j < static_cast<U>(graph.neig[min_ind].size()); ++j) {
-            if (dist[graph.neig[min_ind][j].first] > graph.neig[min_ind][j].second && !used[graph.neig[min_ind][j].first]) {
-                dist[graph.neig[min_ind][j].first] = graph.neig[min_ind][j].second;
-                parent[graph.neig[min_ind][j].first].second = graph.neig[min_ind][j].second;
-                parent[graph.neig[min_ind][j].first].first = min_ind;
+        U min_dist_ind = 0;
+        try {
+            min_dist_ind = find_min(dist, used);
+        }
+        catch (...) {}
+        for (U j = 0; j < static_cast<U>(graph.neig[min_dist_ind].size()); ++j) {
+            if (!used[graph.neig[min_dist_ind][j].first]) {
+                dist.push({ graph.neig[min_dist_ind][j].second, graph.neig[min_dist_ind][j].first });
+                parent[graph.neig[min_dist_ind][j].first].second = graph.neig[min_dist_ind][j].second;
+                parent[graph.neig[min_dist_ind][j].first].first = min_dist_ind;
             }
         }
     }
@@ -112,18 +189,19 @@ Graph<T, U> Prim(Graph<T, U>& graph) {
     return new_graph;
 }
 
+template <typename U>
 struct Dsu {
-    std::vector<int64_t> parent;
-    std::vector<int64_t> rank;
-    Dsu(int64_t n) {
+    std::vector<U> parent;
+    std::vector<U> rank;
+    Dsu(U n) {
         rank.resize(n, 1);
         parent.resize(n, 0);
-        for (int64_t i = 0; i < n; ++i) {
+        for (U i = 0; i < n; ++i) {
             parent[i] = i;
         }
     }
-    int64_t find_parent(int64_t x) {
-        std::vector<int64_t> way;
+    U find_parent(U x) {
+        std::vector<U> way;
         while (parent[x] != x) {
             way.push_back(x);
             x = parent[x];
@@ -133,7 +211,7 @@ struct Dsu {
         }
         return x;
     }
-    void unite(int64_t x, int64_t y) {
+    void unite(U x, U y) {
         x = find_parent(x);
         y = find_parent(y);
         if (rank[x] == rank[y]) {
@@ -156,7 +234,7 @@ template <typename T, typename U>
 Graph<T, U> Kruskal(Edges<T, U>& edges) {
     Edges<T, U> new_edges(edges.vertexCount, 0);
     std::sort(edges.edges.begin(), edges.edges.end(), comp<T, U>);
-    Dsu dsu(edges.vertexCount + 1);
+    Dsu<U> dsu(edges.vertexCount + 1);
     for (auto edge : edges.edges) {
         if (dsu.find_parent(edge.from) != dsu.find_parent(edge.to)) {
             dsu.unite(edge.from, edge.to);
@@ -180,6 +258,8 @@ int main() {
     Graph<int64_t, int64_t> prim_graph = Prim(graph);
     std::cout << "PRIM:\n";
     prim_graph.show();
+
+    std::cout << (kruskal_graph == prim_graph) << "\n";
     /*
     6 7
     1 4 1
